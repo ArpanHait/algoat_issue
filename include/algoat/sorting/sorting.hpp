6a5487ded0e6cff257aaba388294d16478646a64
Sambit Chakraborty <smbtchakraborty@gmail.com>
2026-06-21 13:07:15 +0530

    feat(sorting): define SortAlgorithm concept and variant type

---
+++ include/algoat/sorting/sorting.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <concepts>
+#include <string_view>
+#include <span>
+#include <cstddef>
+#include <variant>
+
+namespace algoat::sorting {
+
+template<typename Algo, typename T>
+concept SortAlgorithm = requires(Algo algo, std::span<T> data) {
+    { algo.name() } -> std::convertible_to<std::string_view>;
+    { algo.sort(data) } -> std::same_as<void>;
+    { algo.preferred_min_size() } -> std::convertible_to<std::size_t>;
+};
+
+using SortVariant = std::variant<std::monostate>;
+
+} // namespace algoat::sorting
