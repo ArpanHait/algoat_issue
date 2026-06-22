e4b5cee9815ad82caaea287314a464fc91903e93
Sambit Chakraborty <smbtchakraborty@gmail.com>
2026-06-22 14:14:09 +0530

    perf(python): optimize list copy overhead for search and sort

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -1,5 +1,4 @@
 #include <nanobind/nanobind.h>
-#include <nanobind/stl/vector.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/optional.h>
 #include <algoat/algoat.hpp>
@@ -8,26 +7,76 @@
 
 namespace nb = nanobind;
 
+template<typename T>
+nb::list sort_list_direct(nb::list data) {
+    size_t n = nb::len(data);
+    std::vector<T> buf(n);
+    for (size_t i = 0; i < n; ++i) {
+        buf[i] = nb::cast<T>(data[i]);
+    }
+    
+    algoat::sort<T>(std::span<T>{buf});
+    
+    nb::list result = nb::steal<nb::list>(PyList_New(n));
+    for (size_t i = 0; i < n; ++i) {
+        PyList_SET_ITEM(result.ptr(), i, nb::cast(buf[i]).release().ptr());
+    }
+    return result;
+}
+
+nb::list sort_dispatch(nb::list data) {
+    if (nb::len(data) == 0) return nb::list();
+    if (PyFloat_Check(data[0].ptr())) {
+        return sort_list_direct<double>(data);
+    } else {
+        return sort_list_direct<int>(data);
+    }
+}
+
+template<typename T>
+std::optional<std::size_t> search_list_direct(nb::list data, const T& target) {
+    std::string algo_name = algoat::get_global_config().searching.prefer.value_or("auto");
+    
+    if (algo_name == "auto" || algo_name == "binarysearch") {
+        size_t lo = 0, hi = nb::len(data);
+        while (lo < hi) {
+            size_t mid = lo + (hi - lo) / 2;
+            T val = nb::cast<T>(data[mid]);
+            if (val == target) return mid;
+            if (val < target) lo = mid + 1;
+            else hi = mid;
+        }
+        return std::nullopt;
+    } else if (algo_name == "linearsearch") {
+        size_t n = nb::len(data);
+        for (size_t i = 0; i < n; ++i) {
+            if (nb::cast<T>(data[i]) == target) return i;
+        }
+        return std::nullopt;
+    } else {
+        std::vector<T> vec(nb::len(data));
+        for (size_t i = 0; i < nb::len(data); ++i) {
+            vec[i] = nb::cast<T>(data[i]);
+        }
+        return algoat::search<T>(std::span<T>{vec}, target);
+    }
+}
+
+std::optional<std::size_t> search_dispatch(nb::list data, nb::object target) {
+    if (nb::len(data) == 0) return std::nullopt;
+    
+    if (PyFloat_Check(data[0].ptr()) || PyFloat_Check(target.ptr())) {
+        return search_list_direct<double>(data, nb::cast<double>(target));
+    } else {
+        return search_list_direct<int>(data, nb::cast<int>(target));
+    }
+}
+
 NB_MODULE(_algoat_impl, m) {
     m.doc() = "Algoat C++ algorithm library bindings";
 
     m.def("load_global_config", &algoat::load_global_config, nb::arg("filepath"), "Load algorithm configuration from a JSON file");
 
-    m.def("sort", [](std::vector<int> data) -> std::vector<int> {
-        algoat::sort<int>(std::span<int>{data});
-        return data;
-    }, nb::arg("data"), "Sort a list of ints");
-
-    m.def("sort", [](std::vector<double> data) -> std::vector<double> {
-        algoat::sort<double>(std::span<double>{data});
-        return data;
-    }, nb::arg("data"), "Sort a list of floats");
-
-    m.def("search", [](std::vector<int> data, const int& target) -> std::optional<std::size_t> {
-        return algoat::search<int>(std::span<int>{data}, target);
-    }, nb::arg("data"), nb::arg("target"), "Search for an int");
-
-    m.def("search", [](std::vector<double> data, const double& target) -> std::optional<std::size_t> {
-        return algoat::search<double>(std::span<double>{data}, target);
-    }, nb::arg("data"), nb::arg("target"), "Search for a float");
+    m.def("sort", &sort_dispatch, nb::arg("data"), "Sort a list of ints or floats");
+    m.def("search", &search_dispatch, nb::arg("data"), nb::arg("target"), "Search for a target in a list");
 }
