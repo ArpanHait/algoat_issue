07c12de5db09a5060282bf3d11d3895ebc18106e
Sambit Chakraborty <smbtchakraborty@gmail.com>
2026-06-24 15:23:12 +0530

    perf(sorting): use branchless cmov for median-of-three pivot selection

--- include/algoat/sorting/introsort.hpp
+++ include/algoat/sorting/introsort.hpp
@@ -20,8 +20,16 @@ struct IntroSort {
     }
 
     template<std::totally_ordered T>
+    static T get_pivot(const T& a, const T& b, const T& c) {
+        // Branchless median-of-three. 
+        // std::min and std::max typically compile down to CMOV (Conditional Move)
+        // instructions on x86 or CSEL on ARM, entirely avoiding branch misprediction penalties.
+        return std::max(std::min(a, b), std::min(std::max(a, b), c));
+    }
+
+    template<std::totally_ordered T>
     static void introsort_impl(std::span<T> data, int depth_limit) {
-        if (data.size() <= 16) {
+        if (data.size() <= 32) {
             InsertionSort{}.sort(data);
             return;
         }
@@ -31,28 +39,24 @@ struct IntroSort {
             return;
         }
 
-        auto pivot = data[data.size() / 2];
-        auto* left = data.data();
-        auto* right = data.data() + data.size() - 1;
-        while (left <= right) {
-            while (*left < pivot) left++;
-            while (pivot < *right) right--;
-            if (left <= right) {
-                std::swap(*left, *right);
-                left++;
-                right--;
-            }
+        // Median-of-three pivot selection
+        auto pivot = get_pivot(data[0], data[data.size() / 2], data[data.size() - 1]);
+        
+        // Hoare partition scheme
+        auto* left = data.data() - 1;
+        auto* right = data.data() + data.size();
+        
+        while (true) {
+            do { left++; } while (*left < pivot);
+            do { right--; } while (pivot < *right);
+            if (left >= right) break;
+            std::swap(*left, *right);
         }
         
-        std::size_t left_size = left - data.data();
-        std::size_t right_size = data.size() - left_size;
+        std::size_t pivot_idx = right - data.data() + 1;
 
-        if (left_size > 0) {
-            introsort_impl(data.subspan(0, left_size), depth_limit - 1);
-        }
-        if (right_size > 0) {
-            introsort_impl(data.subspan(left_size, right_size), depth_limit - 1);
-        }
+        introsort_impl(data.subspan(0, pivot_idx), depth_limit - 1);
+        introsort_impl(data.subspan(pivot_idx, data.size() - pivot_idx), depth_limit - 1);
     }
 
     template<std::totally_ordered T>
